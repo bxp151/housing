011f74d5170d4f280a9419ef22bdfb5a8c05c0df
Brian <bjpietra@ncsu.edu>
2021-01-26 10:18:15 -0500

    014

--- README.md
+++ README.md
@@ -7,69 +7,96 @@ The objective is to predict median home prices in Boston based on data published
 
 The data was provided by U.S. Census service and obtained from the [StatLib Archive](http://lib.stat.cmu.edu/datasets/boston).  It has been used extensively throughout the literature to benchmark algorithms. The data was originally published by Harrison, D. and Rubinfeld, D.L. `Hedonic prices and the demand for clean air', J. Environ. Economics & Management, vol.5, 81-102, 1978. 
 
+</br>
+
 
 ## Performance Measure
-In order to measure the effectiveness of the model in predicting price, we used root mean square error (RMSE).  RMSE is calculated by: 
+In order to measure the effectiveness of the model in predicting housing prices, we used root mean square error (RMSE).  RMSE is calculated by: 
 
 1. calculating the sum the squared differences between the predicted (model) and observed (test set) values 
 2. dividing #1 by the numer of observations
 3. taking the square root of #2
 
-## Constraints
-Talk about how some prices are capped
+</br>
+
 
 ## Key Findings
 
-KF1 - The final model achieved an RMSE of $2,033.18 on the training data and  $1,995.13 on the test data.  The features used in the final model were: 
-	* listings - number of listings
-	* market_location 
-	* latestodometer 
-	* iscertified 
-	* isleather
-	* trimids
-	* age: number of days between startdateofprice and Jaunuary 2016
+1. The final model achieved an RMSE of $2.8K on the training data and  $2.4K on the test (unseen) data
               
-KF2 - The most important feature to predict price by far was trimids. Trimids is so important because it captures the year, make, model, engine and drive type and thus would be directly related to MSRP. 
+2. The most important features to predict price were
+	* **lstat**:  percentage of lower status of the population 
+	* **rm**: average numer of rooms per dwelling 
+
+![](./images/fig1.png)
+
+
+## Model Validation
+In order to validate the model, we used target shuffling which shows the probability that the model's results occured by chance. 
+    
+    For 100K repititions:
+        
+        1. Shuffle the target 
+        2. Fit the best model to the shuffled target (shuffled model)
+        3. Make predictions using the shuffled model and score using RMSE
+        4. Plot the distribution of scores 
+</br>
+
+####Since the best model performed better than every target permutation model, there is a 0 in 100K probability that the model's results occured by chance
+
+![](./images/fig2.png)
+
+
 
 ## Approach
 
-The overall approach to building the pricing model is as follows:
+The overall approach to building the pricing model:
 
 1. Initial data exploration
-2. Select techniques
+2. Select modeling techniques
 3. Split data into Train/Test
 4. Build and analyze baseline models
 5. Feature engineering
-6. Build and analyze secondary models
+6. Build and analyze final models
 7. Final predictions using test set
+
 </br>
 
 ## Data Description
 
 The original data are 506 observations on 14 variables. cmedv is the target variable
 
-Variable | Description
----- | ------------- 
-crim |	per capita crime rate by town
-zn |	proportion of residential land zoned for lots over 25,000 sq.ft
-indus	| proportion of non-retail business acres per town
-chas |	Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
-nox	| nitric oxides concentration (parts per 10 million)
-rm	| average number of rooms per dwelling
-age	| proportion of owner-occupied units built prior to 1940
-dis	| weighted distances to five Boston employment centres
-rad	| index of accessibility to radial highways
-tax	| full-value property-tax rate per USD 10,000
-ptratio	| pupil-teacher ratio by town
-b	1000(B - 0.63)^2 | where B is the proportion of blacks by town
-lstat	| percentage of lower status of the population
-medv	| median value of owner-occupied homes in USD 1000's
-cmedv	| corrected median value of owner-occupied homes in USD 1000's
-town	| name of town
-tract	| census tract
-lon	| longitude of census tract
-lat	| latitude of census tract
+Variable | Description | Modeled
+---- | ----------- | --- 
+crim |	per capita crime rate by town | yes
+zn |	proportion of residential land zoned for lots over 25,000 sq.ft | yes
+indus	| proportion of non-retail business acres per town | yes
+chas |	Charles River dummy variable (= 1 if tract bounds river; 0 otherwise) | yes
+nox	| nitric oxides concentration (parts per 10 million) | yes
+rm	| average number of rooms per dwelling | yes
+age	| proportion of owner-occupied units built prior to 1940 | yes
+dis	| weighted distances to five Boston employment centres | yes
+rad	| index of accessibility to radial highways | yes
+tax	| full-value property-tax rate per USD 10,000 | yes
+ptratio	| pupil-teacher ratio by town | yes
+b	1000(B - 0.63)^2 | where B is the proportion of blacks by town | yes
+lstat	| percentage of lower status of the population | yes
+medv	| median value of owner-occupied homes in USD 1000's | no
+cmedv	| corrected median value of owner-occupied homes in USD 1000's | yes
+town	| name of town | no
+tract	| census tract | no
+lon	| longitude of census tract | yes
+lat	| latitude of census tract | yes
+
+</br>
+
+## Potential Model Improvements
+
+1. Interactions were not explicitly modeled.  The one way interactions between the top 5 important features could be enumerated to see if there is any improvement in the model.
+2. No feature selection was performed.  There is a possibility that we could achieve similiar model performance using a reduced set of features. 
+
+</br>
 
 ## Usage Instructions
 
-Clone the repo by doing ----
+1. Open housing.py and set 
--- housing.py
+++ housing.py
@@ -1,23 +1,44 @@
+
 #%% Imports
+import requests
+import os
 
 import pandas as pd
 import exploretransform as et
 import numpy as np
 import plotnine as pn
-from sklearn.model_selection import StratifiedShuffleSplit
-from pandas.plotting import scatter_matrix
-import requests
-import os
+import plotly.express as px
 import matplotlib.pyplot as plt
+from plotly.offline import plot 
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
+from sklearn.linear_model import GammaRegressor, LinearRegression
+from sklearn.neural_network import MLPRegressor
+import xgboost as xgbr
+
+from sklearn.model_selection import StratifiedShuffleSplit, cross_validate
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import GridSearchCV, cross_val_score, KFold
+from sklearn.metrics import mean_squared_error
+from sklearn.inspection import permutation_importance
 
 #%% Seting Configuration
-
-pd.set_option('display.expand_frame_repr', False)
-pd.set_option('display.max_rows', 500)
-
 HOME_DIR = '/Users/bxp151/ml/housing'
 DATA_DIR = '/data'
+IMG_DIR = '/images'
+
 DATA_URL = 'http://lib.stat.cmu.edu/datasets/boston_corrected.txt'
+
+
+if not os.path.exists("images"):
+    os.mkdir(HOME_DIR + IMG_DIR)
+    
+if not os.path.exists("data"):
+    os.mkdir(HOME_DIR + DATA_DIR)
+    
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.max_rows', 500)
 
 #%% Acquire Dataset
 
@@ -78,8 +99,9 @@ skew.sort_values(by=["skewness"], ascending=False)
 # DIS      float64  1.008779              2-high
 # RAD        int64  1.001833              2-high
 
-# will likley use center, scaling for certain techniques
+# will likley use center, scaling for linear modeling
 
+# plot the target 
 (
  pn.ggplot(data) +
  pn.aes(x = 'CMEDV') +
@@ -87,10 +109,10 @@ skew.sort_values(by=["skewness"], ascending=False)
  pn.labs(x = 'Corrected median price of homes 000s',
          title = 'cmedv normal scale')
 )
-# CMEDV is higly skewed, try GLMs for linear models
 
+# CMEDV is higly skewed, try gaussian and gamma linear models
 
-
+# plot the locations of houses
 data.plot(kind="scatter", x = "LON", y = "LAT", alpha = 0.4, 
         c="CMEDV", cmap=plt.get_cmap("jet"), colorbar=True )
 
@@ -98,21 +120,6 @@ data.plot(kind="scatter", x = "LON", y = "LAT", alpha = 0.4,
 # expensive generally  
 
 
-#%% Select Techniques:
-
-# BASELINES:  
-# Naive (mean) | Random Forest
-
-# SECONDARY:
-# Linear(GLM) + LASSO
-# Linear(GLM) + Elastic
-# Regression Trees
-# Bagging Trees
-# RF
-# AdaBoost
-# Gradient Boost
-# Neural Networks
-# XGBoost
 
 #%% Initial Cleanup
 
@@ -134,8 +141,10 @@ et.peek(df)
 #%% Split data
 
 def strat_split(df, target, quantiles = [0, 0.25,0.5,0.75,1]):
-    # Python doesn't have a built in mechanism for stratification in regression
-    # Specifiy the quantiles and target to create stratified sample
+    '''
+    Python doesn't have a built in mechanism for stratification in regression
+    Specifiy the quantiles and target to create stratified samples
+    '''
     
     # calculate the quantiles for the target
     q = df[target].quantile(quantiles)
@@ -149,8 +158,8 @@ def strat_split(df, target, quantiles = [0, 0.25,0.5,0.75,1]):
     traintest = StratifiedShuffleSplit(n_splits = 1, test_size=0.2, random_state=42)
 
     for train_idx, test_idx in traintest.split(df, df["target_cat"]):
-        train= df.loc[train_idx]
-        test = df.loc[test_idx]
+        train= df.iloc[train_idx]
+        test = df.iloc[test_idx]
 
     train, test = train.drop(['target_cat'], axis = 1), test.drop(['target_cat'], axis = 1)  
 
@@ -159,11 +168,8 @@ def strat_split(df, target, quantiles = [0, 0.25,0.5,0.75,1]):
     
     return trainX, trainY,testX, testY 
 
-# use more quantiles in the upper range due to the rareness of those observations
 trainX, trainY,testX, testY  = strat_split(df, target = "cmedv", 
-                          quantiles = [0, 0.1, 0.2, 0.3, 0.4, 
-                                       0.5, 0.6, 0.7, 0.8, 0.85, 
-                                       0.9, 0.95, 1])
+                          quantiles = [0, 0.25, 0.5, 0.75, 1])
 
 
 # verify cmedv looks similar between train/test
@@ -180,21 +186,325 @@ trainX, trainY,testX, testY  = strat_split(df, target = "cmedv",
  )
 
 
-#%% Naive Baseline - calculate RMSE using mean(cmedv) as the prediction
+#%% Naive Baseline
 
-naive = pd.DataFrame({'obs': trainY,
-                      'pred': np.mean(trainY)
-                      })
-
-naive["residsq"] = (naive["obs"] - naive["pred"])**2
-
-naiveRMSE = np.sqrt(sum(naive["residsq"])/len(naive))
-
-#%% Black box baseline - Use random forest with defaults
-
-# https://scikit-learn.org/stable/modules/ensemble.html#forest
-
-
+def naive():
+    
+    naive = pd.DataFrame({'obs': trainY,
+                          'pred': np.mean(trainY)
+                          })
+    
+    naive["residsq"] = (naive["obs"] - naive["pred"])**2
+    
+    rmse = np.sqrt(sum(naive["residsq"])/len(naive))
+    
+    return np.round(rmse, 2)
 
 
+#%% Black box baseline pipeline and helper functions
 
+num_feat = trainX.select_dtypes('number').columns
+cat_feat = trainX.select_dtypes('category').columns
+
+# numeric
+num = Pipeline([
+    ( "select", et.ColumnSelect(num_feat))
+    ])
+
+# categorical
+cat = Pipeline([
+    ( "select", et.ColumnSelect(cat_feat)),
+    ( 'one_hot',    OneHotEncoder(handle_unknown='ignore') )
+    ])
+
+# combined
+base_pipe = FeatureUnion([ 
+    ('numeric', num), 
+    ('categorical', cat) 
+    ])
+    
+    
+
+def base_rmse(estimator):
+    '''
+    Returns RMSE from cross_validate on specified estimator
+    '''
+    rf_cv = cross_validate(estimator,
+                       X = base_pipe.fit_transform(trainX),
+                       y = trainY, 
+                       scoring='neg_root_mean_squared_error', 
+                       cv = 5)
+
+    rmse = np.mean(-rf_cv['test_score'])
+    
+    return np.round(rmse, 2)
+
+
+#%% Baseline models - non-parametric
+
+# Naive - predicting the mean
+base01_naive = naive()
+
+# Bagging trees - use all features at every split
+base02_bag = base_rmse(RandomForestRegressor(random_state=42, max_features='auto'))
+
+# Random forest - square root of # of features at each split
+base03_rf = base_rmse(RandomForestRegressor(random_state=42, max_features = 'sqrt'))
+
+# Adaboost
+base04_ada = base_rmse(AdaBoostRegressor(random_state=42))
+
+# Gradient Boost
+base05_gb = base_rmse(GradientBoostingRegressor(random_state=42))
+
+# XGBoost
+#dtrain = xgb.DMatrix(base_pipe(trainX).fit_transform(trainX), label = trainY)
+base06_xgb = base_rmse(xgbr.XGBRegressor(random_state=42))
+
+# nueral network - increased max_iter until convergance
+base07_nn = base_rmse(MLPRegressor(random_state=42, max_iter=900))
+
+
+#%% Baseline Linear pipeline and helper functions
+
+# numeric
+num_lin = Pipeline([
+    ( "select", et.ColumnSelect(num_feat)),
+    ( "scale", StandardScaler())
+    ])
+
+# categorical
+cat_lin = Pipeline([
+    ( "select", et.ColumnSelect(cat_feat)),
+    ( 'one_hot',    OneHotEncoder(handle_unknown='ignore') )
+    ])
+
+# combined
+base_pipe_lin = FeatureUnion([ 
+    ('numeric', num_lin), 
+    ('categorical', cat_lin) 
+    ])
+
+
+
+def lin_rmse(estimator):
+    '''
+    Returns RMSE from cross_validate on specified estimator
+    '''
+    rf_cv = cross_validate(estimator,
+                       X = base_pipe_lin.fit_transform(trainX),
+                       y = trainY, 
+                       scoring='neg_root_mean_squared_error', 
+                       cv = 5)
+
+    rmse = np.mean(-rf_cv['test_score'])
+    
+    return np.round(rmse, 2)
+
+#%% Baseline Linear (gaussian, gamma)
+
+# Gamma distribution, ridge
+base08_gr = lin_rmse(GammaRegressor(alpha=0))
+
+# GLinear Regression
+base09_lr = lin_rmse(LinearRegression())
+
+
+#%% Tune and evaluate Gradient Boost
+
+
+# tuning parameters
+params = {'learning_rate': [0.15,0.1,0.05,0.01],
+          'n_estimators':  [50,100,200,400,800,1600]}
+
+# Inner fold for hyperparameter search
+inner_cv = KFold(n_splits=5, shuffle=True, random_state=42)
+
+# Outer fold determines generalization error
+outer_cv = KFold(n_splits=5, shuffle=True, random_state=42)
+
+
+inner = GridSearchCV(estimator=GradientBoostingRegressor(random_state = 42), 
+                  param_grid=params,
+                  cv=inner_cv,
+                  scoring='neg_root_mean_squared_error')
+
+
+outer = cross_val_score(estimator=inner, 
+                        X=base_pipe.fit_transform(trainX), 
+                        y=trainY, 
+                        cv=outer_cv)
+
+outer_rmse = np.mean(-outer) # 2.8 
+
+#%% Fit final model and predict on test set
+
+inner.fit(X = base_pipe.transform(trainX), y = trainY)
+
+inner.best_params_
+
+best_model = inner.best_estimator_  
+
+y_hat = best_model.predict(X = base_pipe.transform(testX))
+
+final_rmse = mean_squared_error(testY, y_hat, squared = False)
+
+#%% Calculate feature importance using feature shuffling
+
+# get column names
+num_cols = np.array(trainX.columns.drop(['chas','rad']))
+cat_cols = cat_lin.steps[1][1].get_feature_names(['chas','rad'])
+all_cols = np.append(num_cols, cat_cols)
+
+
+perm_imp = permutation_importance(best_model, 
+                                  X = base_pipe.transform(testX).toarray(),
+                                  y = testY,
+                                  n_repeats = 100)
+
+
+feat_imp = pd.DataFrame({"features": all_cols,
+                         "importance": perm_imp.importances_mean,
+                         "std" : perm_imp.importances_std}).sort_values \
+                        ("importance",ascending=False)
+
+
+#%% Function to cacluate probability that the model's results occured by chance
+
+def target_shuffling(estimator, trainX, trainY, n_iters, scorefunc, 
+                     random_state=0, verbose = False):
+    '''
+    Model agnostic tehcnique invented by John Elder of Elder Research.  The
+    results show the probability that the model's results occured by chance
+    (p-value)
+    
+    For n_iters:
+        
+        1. Shuffle the target 
+        2. Fit unshuffled input to shuffled target using estimator 
+        3. Make predictions using unshuffled inputs
+        4. Score predictions against shuffled target using scoring function
+        5. Store and return predictions 
+    
+    The distribtuion of scores can be used to plot a histogram in order to 
+    determine p-value
+        
+    Parameters
+    ----------
+    estimator: object
+        A final model estimator that will be evaluated
+
+    trainX : {array-like, sparse matrix} of shape (n_samples, n_features)
+        The training input samples.
+  
+    trainY : array-like of shape (n_samples,)
+        The training target
+            
+    n_iters : int
+        The number of times to shuffle, refit and score model.
+        
+    scorefun : function
+        The scoring function. For example mean_squared_error() 
+        
+    random_state : int, default = None
+        Controls the randomness of the target shuffling. Set for repeatable
+        results  
+    
+    verbose : boolean, default = False
+        The scoring function. For example sklearn.metrics.mean_squared_error() 
+        
+    Returns
+    -------
+    scores : array of shape (n_iters)
+        These are scores calculated for each shuffle
+
+    '''
+ 
+    for i in range(n_iters):    
+        
+        # 1. Shuffle the training target 
+        np.random.default_rng(seed = random_state).shuffle(trainY)
+        random_state += 1
+
+        # 2. Fit unshuffled input to shuffled target using estimator
+        estimator.fit(trainX, trainY)
+        
+        # calculate feature importance using permutation
+        
+        # 3. Make predictions using unshuffled inputs
+        y_hat = estimator.predict(trainX)
+        
+        # 4. Score predictions against shuffled target using scoring function
+        score = scorefunc(trainY, y_hat, squared=False)
+        
+        # 5. Store and return predictions 
+        if i == 0:
+            allscores = np.array(score)
+        else:
+            allscores = np.append(allscores, score)
+        
+        if verbose:
+            print("Shuffle: " + str(i+1) + "\t\tScore: " + str(score))
+    
+    return allscores
+
+
+
+#%% Execute target shuffling
+
+# inner.best_params_['random_state'] = 42 
+
+# gb_estimator = GradientBoostingRegressor(**inner.best_params_)
+
+
+# gb_scores = target_shuffling(estimator = gb_estimator,
+#                  trainX = base_pipe.transform(trainX), 
+#                  trainY = np.array(trainY), 
+#                  n_iters=100000, 
+#                  random_state=0,
+#                  verbose=True,
+#                  scorefunc=mean_squared_error)
+
+# np.save("scores.npy", gb_scores)
+gb_scores = np.load("scores.npy")
+
+#%% Plot feature importance results
+
+top5_feat = feat_imp.iloc[0:5,0:2].sort_values("importance")
+
+fig1 = px.bar(top5_feat, 
+              x="importance",
+              y="features", 
+              title = "Top 5 Features",
+              orientation="h",
+              labels=dict(features=""))
+
+plot(fig1)
+# fig1.write_image(HOME_DIR + IMG_DIR + "/fig1.png")
+
+#%% Plot target shuffling result
+
+fig2 = px.histogram(pd.DataFrame(gb_scores, columns=["RMSE"]), x = "RMSE",
+                    labels=dict(RMSE="RMSE (000s USD)"))
+
+fig2.add_vline(x=final_rmse, line_dash = "dash")
+fig2.update_layout(xaxis_range=[0,6], yaxis_range=[0,1200])
+
+fig2.add_annotation(x=final_rmse, y=1200,
+            text="Best Model",
+            showarrow=True,
+            yshift=5,
+            font=dict(
+                size=16
+                ))
+
+fig2.add_annotation(x=5.2, y=1200,
+            text="Shuffled Models",
+            showarrow=True,
+            yshift=5,
+            font=dict(
+                size=16
+                ))
+
+plot(fig2)
+# fig2.write_image(HOME_DIR + IMG_DIR + "/fig2.png")
